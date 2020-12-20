cba2ab2ffb88a5f0291ec17f113bd4a902bc8f2f
VishnuKumaran <vishnukumaran35@gmail.com>
2020-12-21 06:17:46 +0800

    264810
    264810

---
+++ lib/BookDet.dart
@@ -0,0 +1,17 @@
+class BookDet{
+
+String bookid, booktitle, author, price, description, rating, publisher, isbn, cover;
+
+  BookDet({
+    this.bookid,
+    this.booktitle,
+    this.author,
+    this.price,
+    this.description,
+    this.rating,
+    this.publisher,
+    this.isbn,
+    this.cover,
+  });
+
+}
